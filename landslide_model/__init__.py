779d4f1fabd707db9ec16d6251dcbb5b84d40f77
Gin <142237326+ZeroIsStar@users.noreply.github.com>
2026-05-09 23:13:50 +0800

    Update __init__.py

--- landslide_model/__init__.py
+++ landslide_model/__init__.py
@@ -1,7 +1,7 @@
 from landslide_model.Unet import UNet
-from landslide_model.MFFEnet import DeepLab  # 2022
-from landslide_model.RIPF_Unet import RiPF_UNet  # 2023
-from landslide_model.transUnet2 import TransUNet2  # 2025
-from landslide_model.attention_deeplabv3plus import AttentionDeeplabV3plus  # 2024
-from landslide_model.BFA_ResUnet import BFA_resunet # 2024
+from landslide_model.MFFEnet import DeepLab  
+from landslide_model.RIPF_Unet import RiPF_UNet  
+from landslide_model.transUnet2 import TransUNet2  
+from landslide_model.attention_deeplabv3plus import AttentionDeeplabV3plus  
+from landslide_model.BFA_ResUnet import BFA_resunet 
 from landslide_model.DC_light import *
