9c42ef89f58efc2d131a005c641da0a72fe3588a
Gin <142237326+ZeroIsStar@users.noreply.github.com>
2026-05-09 23:34:30 +0800

    Create configs.py

---
+++ configs/configs.py
@@ -0,0 +1,38 @@
+import torch
+
+cfg = dict(
+    segment_model = dict(
+        type ='MFFEnet',  # ['MFFEnet','RIPF_Unet','Attention_deeplabV3plus','RFA_ResUnet','DC_light_bifpn_cat']
+    ),
+
+    dataset = dict(
+        set          = ['train', 'val', 'test'],
+        dataset_name = '',
+        # ['palu','Mengdong','Hokkaido_Iburi_Tobu','Jiuzhai_valley"_(0.2m)','Longxi_River'(SAT),'Moxi_town"_(0.2m)‘] Seg
+        batch_size   = 4,
+        in_channels= 3,
+        Class=2,
+        # clip_grad_value_ = 5.0  # 模型梯度裁剪
+    ),
+
+    optimizer = dict(
+        type         = 'AdamW',  # ['SGD','AdamW']
+       base_lr       = 2e-4,
+        min_lr       = 1e-6,
+        step_size    = 10,
+        gamma        = 0.9,
+        weight_decay = 5e-4,
+        momentum     = 0.99
+    ),
+    train = dict(
+        loss_function='Tversky_loss_lovasz',  # ['celoss','Tversky_loss_lovasz','f-h-loss', 'lovasz_ce_loss','lovasz_softmax','DynamicWeightedCrossEntropyLoss', 'dynamic_focal_loss']
+        loss_function_weight = torch.tensor([1.0, 1.0]),
+    ),
+
+    scheduler=dict(
+        #['linear', 'step', 'CosineAnnealingLR'] 内置
+        epoch        = 100,
+        type         = 'WarmupCosineAnnealingLR',
+        warmup_epoch = 10
+    )
+)
