05f142792313b2a2341625e7c9337787f61e04c3
Gin <142237326+ZeroIsStar@users.noreply.github.com>
2026-05-09 23:01:29 +0800

    Create __init__.py

---
+++ landslide_moedl/__init__.py
@@ -0,0 +1,18 @@
+from landslide_model.SCDunetpp import SCDUNetPP #2024
+# from landslide_model.BisDeNet import BiSeNet_DenseNet6 #2024
+from landslide_model.bisedenet import BiSeNet_DenseNet6 #2024
+from landslide_model.Unet3P import UNet3Plus_DeepSup
+from landslide_model.Swim_mamba import SwinUMamba
+from landslide_model.landslidemamba import landslidemamba
+from landslide_model.UnetPP_dw import UNet_Nesteddw
+from landslide_model.MLnetpp import RDSSA_net
+from landslide_model.A_new import A_net
+from landslide_model.model import model
+from landslide_model.AMDnet import UNet as AMDnet
+from landslide_model.MFFEnet import DeepLab  # 2022
+from landslide_model.RIPF_Unet import RiPF_UNet  # 2023
+from landslide_model.transUnet2 import TransUNet2  # 2025
+from landslide_model.attention_deeplabv3plus import AttentionDeeplabV3plus  # 2024
+from landslide_model.RFA_ResUnet import BFA_resunet # 2024
+# from landslide_model.RDSSA_net import DC_light
+from landslide_model.DC_light import *
