ff1006bda0b2ee30d3685ef41d3bba8dd8b3f070
Gin <142237326+ZeroIsStar@users.noreply.github.com>
2026-05-09 23:19:32 +0800

    Update __init__.py

--- landslide_model/__init__.py
+++ landslide_model/__init__.py
@@ -3,7 +3,6 @@ from landslide_model.UnetPP import UNet_Nested
 from landslide_model.Segformer import SegFormer
 from landslide_model.MFFEnet import DeepLab  
 from landslide_model.RIPF_Unet import RiPF_UNet  
-from landslide_model.transUnet2 import TransUNet2  
 from landslide_model.attention_deeplabv3plus import AttentionDeeplabV3plus  
 from landslide_model.BFA_ResUnet import BFA_resunet 
 from landslide_model.DC_light import *
