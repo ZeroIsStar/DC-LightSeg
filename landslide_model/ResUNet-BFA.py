e82824a2d80681b03797458a5a6c2586fa43fb19
Gin <142237326+ZeroIsStar@users.noreply.github.com>
2026-05-09 23:09:12 +0800

    Create ResUNet-BFA.py

---
+++ landslide_model/ResUNet-BFA.py
@@ -0,0 +1,425 @@
+########################################################################################################################
+# BFA_ResUnet 2025 https://github.com/zhao952711/landslides_identification/blob/main/Unet_decode.py
+# Landslide Identification from Post-Earthquake High-Resolution Remote Sensing Images Based on ResUNet-BFA
+########################################################################################################################
+
+from collections import OrderedDict
+from typing import Dict
+import torch
+import torch.nn as nn
+from torch import Tensor
+import torch.nn.functional as F
+import cv2
+import numpy as np
+
+########################################################################################################################
+# ResNet50主干网络
+########################################################################################################################
+
+class DoubleConv(nn.Sequential):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        if mid_channels is None:
+            mid_channels = out_channels
+        super(DoubleConv, self).__init__(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up, self).__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        x1 = self.up(x1)
+
+        diff_y = x2.size()[2] - x1.size()[2]
+        diff_x = x2.size()[3] - x1.size()[3]
+
+        # padding_left, padding_right, padding_top, padding_bottom
+        x1 = F.pad(x1, [diff_x // 2, diff_x - diff_x // 2,
+                        diff_y // 2, diff_y - diff_y // 2])
+
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+class Up_B(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up_B, self).__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        diff_y = x2.size()[2] - x1.size()[2]
+        diff_x = x2.size()[3] - x1.size()[3]
+
+        # padding_left, padding_right, padding_top, padding_bottom
+        x1 = F.pad(x1, [diff_x // 2, diff_x - diff_x // 2,
+                        diff_y // 2, diff_y - diff_y // 2])
+
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+class OutConv(nn.Sequential):
+    def __init__(self, in_channels, num_classes):
+        super(OutConv, self).__init__(
+            nn.Conv2d(in_channels, num_classes, kernel_size=1)
+        )
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class Bottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet, self).__init__()
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, x):
+        return self._forward_impl(x)
+
+
+def _resnet(block, layers, **kwargs):
+    model = ResNet(block, layers, **kwargs)
+    return model
+
+def resnet50(**kwargs):
+    r"""ResNet-50 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return _resnet(Bottleneck, [3, 4, 6, 3], **kwargs)
+
+
+class BFA(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(BFA, self).__init__()
+        self.conv1x1 = nn.Conv2d(in_channels + 1, out_channels, kernel_size=1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # 将输入图像转换为灰度图像
+        gray_image = self.to_grayscale(x)
+
+        # 使用Canny算子进行边缘检测
+        edge_image = self.canny_edge_detection(gray_image)
+
+        # 将边缘图像与原图像进行通道级联
+        concatenated_image = torch.cat([x, edge_image], dim=1)
+
+        # 通过1x1卷积动态调整特征权重
+        output = self.conv1x1(concatenated_image)
+        output = self.relu(output)
+
+        return output
+
+    def to_grayscale(self, x):
+        # 将输入图像转换为灰度图像
+        gray_image = 0.2989 * x[:, 0, :, :] + 0.5870 * x[:, 1, :, :] + 0.1140 * x[:, 2, :, :]
+        gray_image = gray_image.unsqueeze(1)
+        return gray_image
+
+    def canny_edge_detection(self, gray_image):
+        # 将灰度图像转换为numpy数组
+        gray_image_np = gray_image.cpu().detach().numpy().astype(np.uint8)
+
+        # 使用Canny算子进行边缘检测
+        edge_image_np = np.zeros_like(gray_image_np)
+        for i in range(gray_image_np.shape[0]):
+            edge_image_np[i, 0] = cv2.Canny(gray_image_np[i, 0], 50, 150)
+
+        # 将边缘图像转换回PyTorch张量
+        edge_image = torch.from_numpy(edge_image_np).float().to(gray_image.device)
+        return edge_image
+
+class IntermediateLayerGetter(nn.ModuleDict):
+    """
+    Module wrapper that returns intermediate layers from a model
+
+    It has a strong assumption that the modules have been registered
+    into the model in the same order as they are used.
+    This means that one should **not** reuse the same nn.Module
+    twice in the forward if you want this to work.
+
+    Additionally, it is only able to query submodules that are directly
+    assigned to the model. So if `model` is passed, `model.feature1` can
+    be returned, but not `model.feature1.layer2`.
+
+    Args:
+        model (nn.Module): model on which we will extract the features
+        return_layers (Dict[name, new_name]): a dict containing the names
+            of the modules for which the activations will be returned as
+            the key of the dict, and the value of the dict is the name
+            of the returned activation (which the user can specify).
+    """
+    _version = 2
+    __annotations__ = {
+        "return_layers": Dict[str, str],
+    }
+
+    def __init__(self, model: nn.Module, return_layers: Dict[str, str]) -> None:
+        if not set(return_layers).issubset([name for name, _ in model.named_children()]):
+            raise ValueError("return_layers are not present in model")
+        orig_return_layers = return_layers
+        return_layers = {str(k): str(v) for k, v in return_layers.items()}
+
+        layers = OrderedDict()
+        for name, module in model.named_children():
+            layers[name] = module
+            if name in return_layers:
+                del return_layers[name]
+            if not return_layers:
+                break
+
+        super(IntermediateLayerGetter, self).__init__(layers)
+        self.return_layers = orig_return_layers
+
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+        out = OrderedDict()
+        for name, module in self.items():
+            x = module(x)
+            if name in self.return_layers:
+                out_name = self.return_layers[name]
+                out[out_name] = x
+        return out
+
+
+class BFA_resunet(nn.Module):
+    def __init__(self, num_classes, pretrain_backbone: bool = False):
+        super(BFA_resunet, self).__init__()
+        backbone = resnet50()
+
+        if pretrain_backbone:
+            backbone.load_state_dict(torch.load("resnet50.pth", map_location='cpu'))
+
+        self.stage_out_channels = [64, 256, 512, 1024, 2048]
+        return_layers = {'relu': 'out0', 'layer1': 'out1', 'layer2': 'out2', 'layer3': 'out3', 'layer4': 'out4'}
+        self.backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+        # 添加BFA模块
+        self.bfa = BFA(in_channels=self.stage_out_channels[4], out_channels=self.stage_out_channels[4])
+
+        c = self.stage_out_channels[4] + self.stage_out_channels[3]
+        self.up1 = Up(c, self.stage_out_channels[3])
+        c = self.stage_out_channels[3] + self.stage_out_channels[2]
+        self.up2 = Up(c, self.stage_out_channels[2])
+        c = self.stage_out_channels[2] + self.stage_out_channels[1]
+        self.up3 = Up(c, self.stage_out_channels[1])
+        c = self.stage_out_channels[1] + self.stage_out_channels[0]
+        self.up4 = Up(c, self.stage_out_channels[0])
+
+        self.conv = OutConv(64, num_classes=num_classes)
+
+    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
+        input_shape = x.shape[-2:]
+        result = OrderedDict()
+        backbone_out = self.backbone(x)
+
+        # BFA模块应用于于最深层的编码层特征图
+        backbone_out['out4'] = self.bfa(backbone_out['out4'])
+
+        x = self.up1(backbone_out['out4'], backbone_out['out3'])
+        x = self.up2(x, backbone_out['out2'])
+        x = self.up3(x, backbone_out['out1'])
+        x = self.up4(x, backbone_out['out0'])
+        x = self.conv(x)
+        x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+        return x
+
+
+if __name__ == "__main__":
+    from thop import profile
+    import time
+    model = BFA_resunet(2).cuda()
+    model.eval()
+    x = torch.rand(1, 3, 256, 256).cuda()
+    output = model(x)
+    flops, params = profile(model, inputs=(x,))
+    num_runs = 10
+    total_time = 0
+    # 多次推理，计算平均推理时间
+    for _ in range(num_runs):
+        start_time = time.time()
+        results = model(x)
+        end_time = time.time()
+        total_time += (end_time - start_time)
+    # 计算平均推理时间
+    avg_inference_time = total_time / num_runs
+    # 计算FPS
+    fps = 1 / avg_inference_time
+    print(f"FPS: {fps:.2f} frames per second")
+    print(f'FLOPs: {flops / 1e9}G')
+    print(f'params: {params / 1e6}M')
+    # model.eval()
+    # input = torch.rand(1, 3, 256, 256).cuda()
+    # output = model(input)
+    # print(output.size())
